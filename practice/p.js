29ae42fbf3f2584321624dd1c39f103632bf4dfb
aking28 <aking28@me.com>
2017-12-06 21:34:15 -0500

    Adding practice

---
+++ practice/p.js
@@ -0,0 +1,5 @@
+let paragraph = document.createElement('p')
+paragraph.innerHTML = 'This is a JS generated practice paragraph tag'
+let body = document.getElementById('content')
+
+body.append(paragraph)
